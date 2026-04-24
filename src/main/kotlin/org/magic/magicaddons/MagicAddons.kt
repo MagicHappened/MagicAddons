6ffcd433932cd77e422a5292a18363c1fb8d2158
BuildTools <unconfigured@null.spigotmc.org>
2026-04-24 21:40:25 +0300

    change to client implementation

--- src/main/kotlin/org/magic/magicaddons/MagicAddons.kt
+++ src/main/kotlin/org/magic/magicaddons/MagicAddons.kt
@@ -1,15 +1,15 @@
 package org.magic.magicaddons
 
 
-import net.fabricmc.api.ModInitializer
+import net.fabricmc.api.ClientModInitializer
 import org.magic.magicaddons.commands.MainCommand
 import org.magic.magicaddons.config.MagicAddonsConfigJsonHandler
 import org.magic.magicaddons.util.ScreenUtil
 import org.magic.magicaddons.util.EntityUtils
 
-class MagicAddons : ModInitializer {
+class MagicAddons : ClientModInitializer {
 
-    override fun onInitialize() {
+    override fun onInitializeClient() {
         EntityUtils
         ScreenUtil.register()
         MainCommand
